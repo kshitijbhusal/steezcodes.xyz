d3258c02ecd363833e6be2b23787a12b01bd3e64
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-01-30 14:15:24 +0545

    test

---
+++ src/components/Work.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <div>
+      <h1>Work</h1>
+      <p>Work</p>
+    </div>
+  );
+};
+
+export default Work;
--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Profile from "../components/Profile";
+import Work from "../components/Work";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <div className="bg-[#18181B] h-screen text-white px-80 py-1">
         <div className="bg-[#18181B] h-full  dark:border-zinc-800 border-zinc-200 border-2 px-4 py-4  ">
           <Profile />
+          <Work />
         </div>
       </div>
     </>
